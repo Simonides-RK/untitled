b7acdc9ce7f1481553dc545c28619253e95419d2
walk1n4money <walkin4money@proton.me>
2025-10-22 10:56:42 +0200

    Create ak47sorte.java

---
+++ src/ak47sorte.java
@@ -0,0 +1,10 @@
+import org.w3c.dom.ls.LSOutput;
+
+import javax.crypto.spec.PSource;
+import java.sql.SQLOutput;
+
+public class ak47sorte {
+    public (String[] args) {
+        System.out.println("Hallo, ich bin ak47sorte");
+    }
+}
